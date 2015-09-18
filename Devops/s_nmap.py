6b34efd5289ddbbbfa402a0686154673abc9ba9a
xiaochuanjiejie <xiaochuanjiejie@163.com>
2015-09-18 14:02:06 +0800

    20150918

--- Devops/s_nmap.py
+++ Devops/s_nmap.py
@@ -21,7 +21,7 @@ except nmap.PortScannerError:
 
 try:
     #’-sS‘代表TCP扫描，‘-sU’代表UDP扫描，参：https://nmap.org/man/zh/man-port-scanning-techniques.html
-    nm.scan(hosts=hosts,arguments=' -v -sS -p '+port)
+    nm.scan(hosts=hosts,arguments=' -v -sU -sS -p '+port)
 except Exception,e:
     print 'Scan error:'+str(e)
 
@@ -29,6 +29,8 @@ for host in nm.all_hosts():
     print '-----------'
     print 'Host : %s (%s)' % (host,nm[host].hostname())
     print 'State : %s' % nm[host].state()
+    print nm.command_line()
+    print nm[host].all_protocols()
     for proto in nm[host].all_protocols():
         print '-----------'
         print 'Protocol : %s' % proto
@@ -37,3 +39,20 @@ for host in nm.all_hosts():
         # print lport
         for port in lport:
             print 'port : %s\tstate : %s' % (port,nm[host][proto][port]['state'])
+'''
+既扫描给定port范围的tcp也扫描udp：
+# python s_nmap.py
+Please input hosts and ports: 123.57.154.241 22-125
+-----------
+Host : 123.57.154.241 ()
+State : up
+nmap -oX - -v -sU -sS -p 22-125 123.57.154.241
+['tcp', 'udp']
+-----------
+Protocol : tcp
+port : 22	state : open
+port : 80	state : open
+-----------
+Protocol : udp
+port : 123	state : open|filtered
+'''
