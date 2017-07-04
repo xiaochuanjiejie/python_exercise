ec214e57e03c645f12bf87f11e6173bfdf28183d
张洁 <xiaochuanjiejie@163.com>
2017-07-05 00:12:07 +0800

    20170705 code

--- Exercise/27/re_18.py
+++ Exercise/27/re_18.py
@@ -11,8 +11,28 @@ with open('/tmp/email.txt','r') as f:
             print '相等'
 
 # 提取出每行中完整的电子邮件地址。
-
 with open('/tmp/email.txt','r') as f:
     for line in f.readlines():
         ss2 = re.search(r'.*::([\w]+@[\w]+\..*)::.*',line)
-        print ss2.group(1)
+        print 'Email: %s' % ss2.group(1)
+
+# 只提取时间戳中的月份
+with open('/tmp/email.txt','r') as f:
+    for line in f.readlines():
+        ss3 = re.search(r'\w+\s(\w+)\s.*',line)
+        print '月份: %s' % ss3.group(1)
+
+# 只提取时间戳中的年份
+with open('/tmp/email.txt','r') as f:
+    for line in f.readlines():
+        ss4 = re.search(r'.*\s(\d{4}).*',line)
+        print '年份: %s' % ss4.group(1)
+
+# 只提取时间戳中的时间
+with open('/tmp/email.txt','r') as f:
+    for line in f.readlines():
+        ss5 = re.search(r'.+(\d\d:\d\d:\d\d)',line)
+        ss6 = re.search(r'.+::(\w+)@(.+)::',line)
+        print '小时分秒: %s' % ss5.group(1)
+        print '登录名: %s;域名: %s' % (ss6.group(1),ss6.group(2))
+
